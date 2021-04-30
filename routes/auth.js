0c99595751673aeb313a7c18e5e4a9d3fdf03927
salogo <zakariasalgo@yahoo.fr>
2021-04-29 19:48:26 -0500

    controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/:message",(req, res) => {
-    res.status(200).send(req.params.message);
-});
+//controllers
+import { showMessage } from "../controllers/auth"
+
+router.get("/:message", showMessage);
 
 
-export default router;
+// export default router;  since we are using required in server.js to import
+//we should use 
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 // we can use import with -r esm npm install esm
 // "start": "nodemon -r esm server.js"
 import express from "express";
-import router from "./routes/auth";
+// import router from "./routes/auth";     replaced with fs....
+import { readdirSync } from "fs";
 
 const app = express();
 
 // route middleware
-app.use("/api",router)
+// fs file system to read all the files from the folder and apply them as a middleware
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)))
+// app.use("/api",router)      replaced with fs...
 
 
 
