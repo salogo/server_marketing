9676c04e7bde4465c0a1176a8bcba925cc639ce7
salogo <zakariasalgo@yahoo.fr>
2021-05-01 16:09:17 -0500

    connecting server to database

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 //const express = require("express");
 import express from "express";//import with -r esm npm install esm{"start": "nodemon -r esm server.js"}
 import { readdirSync } from "fs";
+import cors from "cors";
+import mongoose from "mongoose";
 const morgan = require("morgan");
 require("dotenv").config();
 
 const app = express();
 
+// db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(() => console.log("DB Connected..."))
+.catch((err) => console.log("DB Connection Error", err));
+
 // middlewares
+app.use(cors());
 app.use(morgan("dev"));
 
 
